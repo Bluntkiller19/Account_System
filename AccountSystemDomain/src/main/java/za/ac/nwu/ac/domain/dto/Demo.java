f573f3eccaf5bd919fd77cf07a319c17cd69a270
Bluntkiller19 <mstroebel19@gmail.com>
2021-09-29 14:08:07 +0200

    Test

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/dto/Demo.java
@@ -0,0 +1,9 @@
+package za.ac.nwu.ac.domain.dto;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo {
+
+}
---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/service/GeneralResponse.java
@@ -0,0 +1,7 @@
+package za.ac.nwu.ac.domain.service;
+
+public class GeneralResponse<S> {
+
+    public GeneralResponse(boolean successful, S payload) {
+    }
+}
