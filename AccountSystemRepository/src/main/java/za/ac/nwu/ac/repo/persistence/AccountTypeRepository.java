f4f6baf8c272cc44d56169ca02f75888b1eab6da
Bluntkiller19 <mstroebel19@gmail.com>
2021-10-07 19:15:32 +0200

    query native query working

--- AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
+++ AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
@@ -1,6 +1,5 @@
 package za.ac.nwu.ac.repo.persistence;
 
-import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,13 +9,13 @@ import za.ac.nwu.ac.domain.persistence.AccountType;
 public interface AccountTypeRepository extends JpaRepository<AccountType, Long> {
 
 
-    @Query(value = "SELECT " +
-            "ACCOUNT_TYPE_ID, " +
-            "ACCOUNT_TYPE_NAME, " +
-            "CREATION_DATE, " +
-            "MNEMONIC" +
-            "FROM  " +
-            "C##.ACCOUNT_TYPE " +
-            "WHERE MNEMONIC = :mnemonic ", nativeQuery = true)
+    @Query(value = " SELECT " +
+            " ACCOUNT_TYPE_ID, " +
+            " ACCOUNT_TYPE_NAME, " +
+            " CREATION_DATE, " +
+            " MNEMONIC " +
+            " FROM  " +
+            " C##NEW.ACCOUNT_TYPE " +
+            " WHERE MNEMONIC = :mnemonic ", nativeQuery = true)
     AccountType getAccountTypeByMnemonicNativeQuery(String mnemonic);
 }
--- AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTypeTranslator.java
+++ AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTypeTranslator.java
@@ -5,6 +5,7 @@ import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import java.util.List;
 
 public interface AccountTypeTranslator {
+
     List<AccountTypeDto> getAllAccountTypes();
 
     AccountTypeDto create(AccountTypeDto accountType);
